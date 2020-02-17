ba83aac0cc60a2e7a3e59049d600af046d5f4438
kato sho <nascoqq@gmail.com>
2020-02-17 15:17:34 +0900

    localstorageの掲示板完成

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h2>Cats</h2>
+    <div v-for="(cat, n) in cats" :key="n">
+      <p>
+        <span class="cat">{{ cat }}</span>
+        <button @click="removeCat(n)">Remove</button>
+      </p>
+    </div>
+    <p>
+      <input v-model="newCat" />
+      <button @click="addCat">Add Cat</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cats: [],
+      newCat: ''
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('cats')) {
+      try {
+        this.cats = JSON.parse(localStorage.getItem('cats'))
+      } catch (e) {
+        localStorage.removeItem('cats')
+      }
+    }
+  },
+  methods: {
+    addCat() {
+      // ensure they actually typed something
+      if (!this.newCat) return
+      this.cats.push(this.newCat)
+      this.newCat = ''
+      this.saveCats()
+    },
+    removeCat(x) {
+      this.cats.splice(x, 1)
+      this.saveCats()
+    },
+    saveCats() {
+      const parsed = JSON.stringify(this.cats)
+      localStorage.setItem('cats', parsed)
+    }
+  }
+}
+</script>
