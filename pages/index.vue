ef5da35a464d692061c40ea594e70a8bd137a71a
kato sho <nascoqq@gmail.com>
2020-02-18 12:32:44 +0900

    これからfirestoreに置き換え

--- pages/index.vue
+++ pages/index.vue
@@ -13,40 +13,40 @@
     </p>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      cats: [],
-      newCat: ''
-    }
-  },
-  mounted() {
-    if (localStorage.getItem('cats')) {
-      try {
-        this.cats = JSON.parse(localStorage.getItem('cats'))
-      } catch (e) {
-        localStorage.removeItem('cats')
-      }
-    }
-  },
-  methods: {
-    addCat() {
-      // ensure they actually typed something
-      if (!this.newCat) return
-      this.cats.push(this.newCat)
-      this.newCat = ''
-      this.saveCats()
-    },
-    removeCat(x) {
-      this.cats.splice(x, 1)
-      this.saveCats()
-    },
-    saveCats() {
-      const parsed = JSON.stringify(this.cats)
-      localStorage.setItem('cats', parsed)
-    }
-  }
-}
-</script>
+<!--<script>-->
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      cats: [],-->
+<!--      newCat: ''-->
+<!--    }-->
+<!--  },-->
+<!--  // DOM生成直後-->
+<!--  mounted() {-->
+<!--    if (localStorage.getItem('cats')) {-->
+<!--      try {-->
+<!--        this.cats = JSON.parse(localStorage.getItem('cats'))-->
+<!--      } catch (e) {-->
+<!--        localStorage.removeItem('cats')-->
+<!--      }-->
+<!--    }-->
+<!--  },-->
+<!--  methods: {-->
+<!--    addCat() {-->
+<!--      // ensure they actually typed something-->
+<!--      if (!this.newCat) return-->
+<!--      this.cats.push(this.newCat)-->
+<!--      this.newCat = ''-->
+<!--      this.saveCats()-->
+<!--    },-->
+<!--    removeCat(x) {-->
+<!--      this.cats.splice(x, 1)-->
+<!--      this.saveCats()-->
+<!--    },-->
+<!--    saveCats() {-->
+<!--      const parsed = JSON.stringify(this.cats)-->
+<!--      localStorage.setItem('cats', parsed)-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
---
+++ plugins/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUmUvxF3Ns9d5WY4Krtsp0pRb2coTAg_A',
+  authDomain: 'firestore-3a9e6.firebaseapp.com',
+  databaseURL: 'https://firestore-3a9e6.firebaseio.com',
+  projectId: 'firestore-3a9e6',
+  storageBucket: 'firestore-3a9e6.appspot.com',
+  messagingSenderId: '438266842909',
+  appId: '1:438266842909:web:29a6116283ef0992229bf4',
+  measurementId: 'G-6GMESK99WC'
+}
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
